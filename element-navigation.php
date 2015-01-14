dd0f8658de12e01a797f4832c98c5f5dc9a35437
grayayer <grayayer@gmail.com>
2015-01-14 11:10:52 -0800

    removes responsive navigation

--- element-navigation.php
+++ element-navigation.php
@@ -1,11 +1,8 @@
 		<!-- Menu -->
 		<div class="twelve columns omega" id="menu"> 
-						
 			<div class="navigation">
-				
 				<!-- DEFAULT NAVIGATION -->
 				<?php 
-					
 				wp_nav_menu( array(
 					 'container' =>false,
 					 'theme_location' => 'topbar',
@@ -18,38 +15,8 @@
 					 'depth' => 0,
 					 'walker' => new description_walker())
 				 ); 
-				 	
 				?>
-				 <!-- /DEFAULT NAVIGATION -->
-				 
-				 
-				 <!-- RESPONSIVE NAVIGATION FLIP -->
-				<form id="responsive-nav" action="#" method="post">
-				<select class="responsive-nav-select">
-				<option value="">Navigation</option>
-				<?php 
-				
-				$menu = wp_nav_menu(array('theme_location' => 'topbar_small', 'echo' => false));
-				   if (preg_match_all('#(<a [^<]+</a>)#',$menu,$matches)) {
-				      $hrefpat = '/(href *= *([\"\']?)([^\"\' ]+)\2)/';
-				      foreach ($matches[0] as $link) {
-				         // Do something with the link
-				         if (preg_match($hrefpat,$link,$hrefs)) {
-				            $href = $hrefs[3];
-				         }
-				         if (preg_match('#>([^<]+)<#',$link,$names)) {
-				            $name = $names[1];
-				         }
-				         echo "<option value=\"$href\">$name</option>";
-				      }
-				   }				
-				
-				?>
-				</select>
-				</form>
-				<!-- /END RESPONSIVE NAV -->
-							 
+				 <!-- /DEFAULT NAVIGATION -->							 
 			</div>	
-			
 		</div>		 
 		<!-- /End Menu -->
